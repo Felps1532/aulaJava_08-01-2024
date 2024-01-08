52c82a271625d7a0376138b19845388440b2ee32
Felipe Belomi <86834912+Felps1532@users.noreply.github.com>
2024-01-08 16:56:32 -0300

    Add files via upload

---
+++ booleanJava/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(ImprimirTextoMaiusculo("Hello, World!" + 1+6*7));
+    }
+    public static String ImprimirTextoMaiusculo(String Texto){
+        return Texto.toUpperCase();
+    }
+}
